33952915c2a60a00abba58418ac58cf3e368cd9d
ajfrierson <alvinfrierson@fuller.edu>
2019-02-19 12:52:41 -0800

    added api get requests including an api request by id.

--- index.js
+++ index.js
@@ -11,7 +11,25 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+//list all zoos
+sever.get('api/zoos', async (req, res) => {
+  try {
+    const zoos = await db('zoos')
+    res.status(200).json(zoos);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
+//list zoos by id
+server.get('./api/zooz/:id', async (req, res) => {
+  try {
+    const zoos = await db('zoos').where({ id: req.params.id });
+    res.status(200).json(zoos); 
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 const port = 3300;
 server.listen(port, function() {
